2521d74f7d197688587d2c2178da7d7c95d53d7a
spw5235 <spwisner@gmail.com>
2016-12-25 18:49:30 -0500

    comleted assignment nine

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,6 +1,11 @@
 
-var cardOne = "10";
-var cardTwo = "8";
-var cardThree = "1";
-var cardFour = "2";
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
 
+if (cardOne === cardFour || cardTwo === cardThree) {
+	alert("You found a match");
+} else {
+	alert("Sorry, try again");
+}
