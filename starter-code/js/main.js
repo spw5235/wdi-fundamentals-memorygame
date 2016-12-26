4ba3718ab481ebb74ff6e3ba2f211c1736a97d04
Steve Wisner <spwisner@gmail.com>
2016-12-26 14:13:01 -0500

    assign ten attempt

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -4,8 +4,23 @@ var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
 
+/*
 if (cardOne === cardFour || cardTwo === cardThree) {
 	alert("You found a match");
 } else {
 	alert("Sorry, try again");
 }
+*/
+
+var gameBoardID = document.getElementById("game-board");
+
+function createCards() {
+	for ( i = 0; i < 4; i++ ) {
+		var cardDiv = document.createElement('div');
+		cardDiv.className = 'card';
+		cardDiv.innerHTML = '';
+		gameBoardID.appendChild(cardDiv);
+	}
+}
+
+createCards();
